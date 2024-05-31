e0baf822c8e6ad1ac04a764cbe5f0d41764ea9f7
Jackwio <will582145@gmail.com>
2024-05-31 19:39:28 +0800

    739

--- src/main/java/com/my/foodsafe/controllers/RestaurantController.java
+++ src/main/java/com/my/foodsafe/controllers/RestaurantController.java
@@ -5,6 +5,8 @@ import com.my.foodsafe.services.IRestaurantService;
 import com.my.foodsafe.utilities.IUUIDGenerator;
 import com.my.foodsafe.utilities.TestUpload;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -32,5 +34,28 @@ class RestaurantController {
         testUpload.savePhotoLocally("AUGGfZnHB5Ue4Gj8-VycVDqXtAZ6f6hIg9z7AN5fpolColO0EnuDjBGQ_3IAEjLm7Ji88CTjRNdaW1HutDHeGbgExn9hddXuf1_BkftRUS22ORrA2nnngpER3t32G5mEwmYgV7iSz2U3A4iMj8l5COw7O6LHiykVSbrNarLlLY0YJ3Bote9L");
         System.out.println("ddd");
         return restaurant;
+    }
+
+    @PostMapping("/scan")
+    public ResponseEntity<?> uploadImage(@RequestParam("image") MultipartFile file) {
+        try {
+            // 檢查上傳的檔案是否存在
+            if (file.isEmpty()) {
+                return ResponseEntity.badRequest().body("File is empty");
+            }
+
+            // 創建上傳目錄(如果不存在)
+//            Path uploadPath = Paths.get(UPLOAD_DIRECTORY);
+//            Files.createDirectories(uploadPath);
+//
+//            // 保存上傳的檔案
+//            byte[] bytes = file.getBytes();
+//            Path filePath = uploadPath.resolve(file.getOriginalFilename());
+//            Files.write(filePath, bytes);
+
+            return ResponseEntity.ok("File uploaded successfully");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to upload file");
+        }
     }
 }
--- src/main/java/com/my/foodsafe/services/RestaurantImpl.java
+++ src/main/java/com/my/foodsafe/services/RestaurantImpl.java
@@ -4,7 +4,11 @@ import com.my.foodsafe.pojo.Restaurant;
 import com.my.foodsafe.utilities.IImageUtility;
 import com.my.foodsafe.utilities.IUUIDGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
